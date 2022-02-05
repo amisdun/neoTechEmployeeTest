f54a097e81951639f9a3826be47d712ee19ecd57
amisdun <michealdunamis@gmail.com>
2022-02-05 20:15:51 +0000

    defined entry point of the project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Employees Application')
+    .setDescription('Employees API Application')
+    .setVersion('v1')
+    .addTag('employees')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
